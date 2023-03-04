a7cb065ae8e30b1c7c559bd78f6865d75d987cb1
Sphesihle Madonsela <sphesihlemanuel99@gmail.com>
2023-03-05 01:48:24 +0200

    Functions as data

--- src/main/java/org/sphedev/FunctionInterfaces.java
+++ src/main/java/org/sphedev/FunctionInterfaces.java
@@ -1,6 +1,36 @@
 package org.sphedev;
+import java.util.Optional;
 import java.util.function.Consumer;
 public class FunctionInterfaces {
+
+    protected static class Person{
+        private  String name;
+        private  Integer age;
+
+        public Person(String name, Integer age){
+            this.name = name;
+            this.age = age;
+        }
+        public String toString(){
+            return this.name +" "+this.age;
+        }
+    }
+
+    protected static class DataLoader{
+        public final NoArgsFunction<Person> select ;
+
+        public DataLoader(Boolean isDevelopment){
+            this.select = isDevelopment ? this::loadPersonFake : this::loadPersonReal;
+        }
+        private Person loadPersonReal(){
+            System.out.println("Loading person....");
+            return new Person("Real Person",23);
+        }
+        private Person loadPersonFake(){
+            System.out.println("Loading person....");
+            return new Person("Fake Person",47);
+        }
+    }
     public static void main(String[] args) {
         Consumer<String> cout = System.out::println;
         // 3 args
@@ -11,5 +41,10 @@ public class FunctionInterfaces {
         NoArgsFunction<String> print = () -> "Hello no args";
         // print
         cout.accept(print.apply());
+
+        final Boolean IS_DEV = true;
+
+        DataLoader dataLoader = new DataLoader(IS_DEV);
+        cout.accept(dataLoader.select.apply().toString());
     }
 }
